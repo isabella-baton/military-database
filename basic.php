94c1f565041c74e942562508a89c978d90ac3f1e
isabella-baton <isabella_baton@uri.edu>
2025-04-20 11:02:00 -0400

    set up basic site

---
+++ basic.php
@@ -0,0 +1,36 @@
+<?php
+
+    require 'includes/database-connection.php';
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+		<meta charset="UTF-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<title>Military Database</title>
+  		<link rel="stylesheet" href="css/basic.css">
+	</head>
+
+    <body>
+
+        <header>
+
+            <nav>
+                <ul>
+                    <li><a href = "login.php"> Back to Login </a></li>
+                </ul>
+            </nav>
+
+        </header>
+
+        <main>
+
+        </main>
+
+    </body>
+
+</html?
